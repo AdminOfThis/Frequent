5ddf78dcee3efe7de5ea41a74235c5de05815178
AdminOfThis <florianhild95@gmail.com>
2018-05-04 19:50:20 +0200

    Working Channel Cells

--- src/gui/utilities/controller/VuMeter.java
+++ src/gui/utilities/controller/VuMeter.java
@@ -21,12 +21,9 @@ import javafx.util.Duration;
 
 public class VuMeter extends AnchorPane implements Initializable, LevelObserver {
 
-
 	private static final String	FXML_VERTICAL	= "/gui/utilities/gui/VuMeterVertical.fxml";
 	private static final String	FXML_HORIZONTAL	= "/gui/utilities/gui/VuMeterHorizontal.fxml";
-	private static final int	REFRESH_RATE	= 25;
-
-
+	private static final int	REFRESH_RATE	= 50;
 	@FXML
 	private StackPane			vuPane;
 	@FXML
@@ -35,12 +32,10 @@ public class VuMeter extends AnchorPane implements Initializable, LevelObserver 
 	private Label				lblPeak;
 	@FXML
 	private AnchorPane			clippingPane;
-
 	private Channel				channel;
 	private Timeline			line;
 	private double				peak			= 0;
 	private Orientation			orientation;
-
 
 	public VuMeter(Channel channel, Orientation o) {
 		this.orientation = o;
@@ -57,51 +52,50 @@ public class VuMeter extends AnchorPane implements Initializable, LevelObserver 
 		AnchorPane.setBottomAnchor(p, 0.0);
 		AnchorPane.setLeftAnchor(p, 0.0);
 		AnchorPane.setRightAnchor(p, 0.0);
-
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initTimeline();
-
-
 	}
 
 	@Override
-	public void levelChanged(double level) {
-
-	}
-
+	public void levelChanged(double level) {}
 
 	private void initTimeline() {
 		line = new Timeline();
 		KeyFrame frame = new KeyFrame(Duration.millis(REFRESH_RATE), e -> {
 			if (channel != null) {
 				double peakdB = Channel.percentToDB(channel.getLevel() * 1000.0);
-
 				if (peak < peakdB) {
 					peak = peakdB;
 				}
 				vuPeakPane.setStyle("-fx-background-color: linear-gradient( to bottom, -fx-accent, derive(-fx-accent, -50%) );");
 				if (orientation == Orientation.VERTICAL) {
-					vuPeakPane
-						.setPrefHeight(vuPane.getHeight() * (peakdB + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
-					vuLastPeakPane
-						.setPrefHeight(vuPane.getHeight() * (peak + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
+					vuPeakPane.setPrefHeight(vuPane.getHeight() * (peakdB + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
+					double height = vuPane.getHeight() * (peak + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN);
+// System.out.println(peak);
+					vuLastPeakPane.setPrefHeight(height);
 				} else {
-					vuPeakPane
-						.setPrefWidth(vuPane.getWidth() * (peakdB + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
-					vuLastPeakPane
-						.setPrefWidth(vuPane.getWidth() * (peak + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
-
+					vuPeakPane.setPrefWidth(vuPane.getWidth() * (peakdB + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
+					vuLastPeakPane.setPrefWidth(vuPane.getWidth() * (peak + Math.abs(FFTController.FFT_MIN)) / Math.abs(FFTController.FFT_MIN));
 				}
-
-				lblPeak.setText(Math.round(peakdB * 10.0) / 10.0 + "");
-				if (peakdB >= 0.99) {
+				if (peakdB >= FFTController.FFT_MIN) {
+					lblPeak.setText(Math.round(peakdB * 10.0) / 10 + "");
+				} else {
+					lblPeak.setText("-\u221E");
+				}
+				if (peakdB >= -0.5) {
 					clippingPane.setStyle("-fx-background-color: red");
+				} else if (peakdB >= -2.0) {
+					clippingPane.setStyle("-fx-background-color: yellow");
 				} else {
 					clippingPane.setStyle("");
 				}
+				if (peak == 0.0) {
+					peak = -1.0;
+				}
+				peak = 1.2 * peak;
 			}
 		});
 		line.getKeyFrames().add(frame);
@@ -116,6 +110,4 @@ public class VuMeter extends AnchorPane implements Initializable, LevelObserver 
 	public Channel getChannel() {
 		return channel;
 	}
-
-
 }
