898599cd87ea8035993a3c4b48f54ac493a9c13e
AdminOfThis <florianhild95@gmx.de>
2018-05-17 22:48:32 +0200

    Increased to Version 0.0.4

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
 	private static Logger		LOG;
 	public static final String	TITLE			= "Frequent";
-	private static final String	VERSION			= "0.0.3";
+	private static final String	VERSION			= "0.0.4";
 	private static final String	LOG_CONFIG_FILE	= "./log4j.ini";
 	private static final String	GUI_IO_CHOOSER	= "/gui/gui/IOChooser.fxml";
 	private static String		style			= "";
