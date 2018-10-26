eb5a431d6d7c9c6690abc7a2dca4bdf494cd00cb
AdminOfThis <florianhild95@gmail.com>
2018-10-26 13:57:06 +0200

    Added some tests

--- src/test/java/data/ChannelTest.java
+++ src/test/java/data/ChannelTest.java
@@ -3,6 +3,7 @@ package data;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,10 +16,14 @@ public class ChannelTest {
 	@Test
 	@BeforeEach
 	public void createChannel() {
+		// Channel 1
 		c1 = new Channel("Channel 1");
-		c2 = new Channel("Channel 2");
 		assertNotNull(c1);
+		assertEquals("Channel 1", c1.getName());
+		// Channel 2
+		c2 = new Channel("Channel 2");
 		assertNotNull(c2);
+		assertEquals("Channel 2", c2.getName());
 	}
 
 	@Test
@@ -28,5 +33,11 @@ public class ChannelTest {
 		c1.setStereoChannel(c2);
 		assertEquals(c1.getStereoChannel(), c2);
 		assertEquals(c2.getStereoChannel(), c1);
+	}
+
+	@Test
+	public void equals() {
+		c2.setName(c1.getName());
+		assertTrue(c1.equals(c2));
 	}
 }
