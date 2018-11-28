b2ed519d66fc3c0899388b2b017387cbe68cac8a
AdminOfThis <florianhild95@gmx.de>
2018-11-28 13:01:09 +0100

    Added Main Test to see if application launches

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,39 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	public void launchApplication() {
+
+		try {
+			Thread thread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+
+					try {
+						Main.main(new String[] { "-debug" });
+					} catch (Exception e) {
+						e.printStackTrace();
+						fail(e.getMessage());
+					}
+
+				}
+			});
+			thread.start();// Initialize the thread
+			Thread.sleep(10000);
+			Main.close();
+			Thread.sleep(5000);
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		return;
+	}
+
+
+}
