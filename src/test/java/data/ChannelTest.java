be1015a43970837309ec624ad943453d9b3a35e1
AdminOfThis <florianhild95@gmail.com>
2020-01-13 14:53:07 +0100

    Moar coverage

--- src/test/java/data/ChannelTest.java
+++ src/test/java/data/ChannelTest.java
@@ -61,6 +61,7 @@ public class ChannelTest {
 		assertFalse(channel[1].isStereo());
 		assertFalse(channel[2].isStereo());
 		channel[0].setStereoChannel(channel[1]);
+		channel[0].setStereoChannel(channel[1]);
 		assertEquals(channel[0].getStereoChannel(), channel[1]);
 		assertEquals(channel[1].getStereoChannel(), channel[0]);
 		assertTrue(channel[0].isStereo());
